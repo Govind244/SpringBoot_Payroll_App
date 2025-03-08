7de59a44e93e4c877c4f7fcd7594b9a08d9ead1e
Govind244 <govindsaraswat244@gmail.com>
2025-03-08 23:53:03 +0530

    lombok_logging& set env.

--- src/main/java/com/bridgelabz/EmployeePayrollApp/model/Employee.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/model/Employee.java
@@ -1,10 +1,66 @@
+//package com.bridgelabz.EmployeePayrollApp.model;
+//
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.*;
+//
+//@Entity
+//@Table(name = "employees")
+//public class Employee {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @NotBlank(message = "Name cannot be empty")
+//    private String name;
+//
+//    @Min(value = 10000, message = "Salary must be at least 10,000")
+//    private double salary;
+//
+//    public Employee() {}
+//
+//    public Employee(String name, double salary) {
+//        this.name = name;
+//        this.salary = salary;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public double getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(double salary) {
+//        this.salary = salary;
+//    }
+//}
+
+
 package com.bridgelabz.EmployeePayrollApp.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Entity
 @Table(name = "employees")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
@@ -16,35 +72,5 @@ public class Employee {
 
     @Min(value = 10000, message = "Salary must be at least 10,000")
     private double salary;
-
-    public Employee() {}
-
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
+
--- src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeeServiceImpl.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApp/service/EmployeeServiceImpl.java
@@ -1,3 +1,62 @@
+//package com.bridgelabz.EmployeePayrollApp.service;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.bridgelabz.EmployeePayrollApp.dto.EmployeeDTO;
+//import com.bridgelabz.EmployeePayrollApp.exception.EmployeeNotFoundException;
+//import com.bridgelabz.EmployeePayrollApp.model.Employee;
+//import com.bridgelabz.EmployeePayrollApp.repository.EmployeeRepository;
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@Service
+//public class EmployeeServiceImpl implements EmployeeService {
+//
+//    @Autowired
+//    private EmployeeRepository employeeRepository;
+//
+//    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+//        this.employeeRepository = employeeRepository;
+//    }
+//
+//
+//    @Override
+//    public List<Employee> getAllEmployees() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    @Override
+//    public Employee getEmployeeById(Long id) {
+//        return employeeRepository.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with ID: " + id));
+//    }
+//
+//    @Override
+//    public Employee addEmployee(EmployeeDTO employeeDTO) {
+//        Employee employee = new Employee();
+//        employee.setName(employeeDTO.name());
+//        employee.setSalary(employeeDTO.salary());
+//        return employeeRepository.save(employee);
+//    }
+//
+//    @Override
+//    public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) {
+//        Employee employee = getEmployeeById(id);
+//        employee.setName(employeeDTO.name());
+//        employee.setSalary(employeeDTO.salary());
+//        return employeeRepository.save(employee);
+//    }
+//
+//    @Override
+//    public void deleteEmployee(Long id) {
+//        employeeRepository.deleteById(id);
+//    }
+//}
+
+
 package com.bridgelabz.EmployeePayrollApp.service;
 
 import java.util.List;
@@ -8,22 +67,33 @@ import com.bridgelabz.EmployeePayrollApp.dto.EmployeeDTO;
 import com.bridgelabz.EmployeePayrollApp.exception.EmployeeNotFoundException;
 import com.bridgelabz.EmployeePayrollApp.model.Employee;
 import com.bridgelabz.EmployeePayrollApp.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @Override
     public List<Employee> getAllEmployees() {
+        log.info("Fetching all employees");
         return employeeRepository.findAll();
     }
 
     @Override
     public Employee getEmployeeById(Long id) {
+        log.info("Fetching employee with ID: {}", id);
         return employeeRepository.findById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with ID: " + id));
+                .orElseThrow(() -> {
+                    log.error("Employee not found with ID: {}", id);
+                    return new EmployeeNotFoundException("Employee not found with ID: " + id);
+                });
     }
 
     @Override
@@ -31,12 +101,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee();
         employee.setName(employeeDTO.name());
         employee.setSalary(employeeDTO.salary());
+        log.info("Adding new employee: {}", employee);
         return employeeRepository.save(employee);
     }
 
     @Override
     public Employee updateEmployee(Long id, EmployeeDTO employeeDTO) {
         Employee employee = getEmployeeById(id);
+        log.info("Updating employee with ID: {}", id);
         employee.setName(employeeDTO.name());
         employee.setSalary(employeeDTO.salary());
         return employeeRepository.save(employee);
@@ -44,6 +116,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public void deleteEmployee(Long id) {
+        log.warn("Deleting employee with ID: {}", id);
         employeeRepository.deleteById(id);
     }
 }
